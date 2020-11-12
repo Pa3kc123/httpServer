5b6e05e982c98cc72ea83e6bf0cd7c08842b4906
patrik.capay <patrik.capay@kios.sk>
2020-11-12 16:02:08 +0100

    Update
    Trying out my json library (this commit os only for testing purposes)

--- src/main/kotlin/sk/pa3kc/miniprojects/App.kt
+++ src/main/kotlin/sk/pa3kc/miniprojects/App.kt
@@ -1,6 +1,8 @@
 package sk.pa3kc.miniprojects
 
+import jdk.nashorn.internal.parser.JSONParser
 import sk.pa3kc.miniprojects.thread.http.HttpServerImpl
+import sk.pa3kc.mylibrary.json.JsonParser
 import java.io.File
 
 class App {
@@ -9,31 +11,38 @@ class App {
 
         @JvmStatic
         fun main(args: Array<String>) {
-            with(File(CSV_DIR_PATH)) {
-                if (exists()) {
-                    if (isFile) {
-                        throw IllegalStateException("Cannot create csv directory - exists file with same name")
-                    }
-                } else {
-                    if (!mkdir()) {
-                        throw IllegalStateException("Cannot create csv directory - directory probably exists")
-                    }
-                }
+            if (args.isEmpty()) return;
+
+            File(args[0]).also {
+                val jsonObj = JsonParser.decodeJsonObject(it.readText())
+                println(jsonObj)
             }
 
-            if ("--preload" in args) {
-                AppConfig
-            }
-
-            HttpServerThread.settings {
-                this.defaults {
-                    headers["Connection"] = "close"
-                }
-
-                this.dirServing("site1")
-            }
-
-            HttpServerThread.initialized = true
+//            with(File(CSV_DIR_PATH)) {
+//                if (exists()) {
+//                    if (isFile) {
+//                        throw IllegalStateException("Cannot create csv directory - exists file with same name")
+//                    }
+//                } else {
+//                    if (!mkdir()) {
+//                        throw IllegalStateException("Cannot create csv directory - directory probably exists")
+//                    }
+//                }
+//            }
+//
+//            if ("--preload" in args) {
+//                AppConfig
+//            }
+//
+//            HttpServerThread.settings {
+//                this.defaults {
+//                    headers["Connection"] = "close"
+//                }
+//
+//                this.dirServing("site1")
+//            }
+//
+//            HttpServerThread.initialized = true
         }
     }
 }
